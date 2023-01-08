10678710963aabede904b3a86456898fec1775d7
Zinmus <alex.zinmus@gmail.com>
2023-01-08 01:33:01 +0100

    Formatted code

--- src/models/Folder.ts
+++ src/models/Folder.ts
@@ -1,72 +1,76 @@
-import { Model } from "@vuex-orm/core";
-import FolderPhoto from "./FolderPhoto";
-import Photo from "./Photo";
+import { Model } from '@vuex-orm/core'
+import FolderPhoto from './FolderPhoto'
+import Photo from './Photo'
 
 export default class Folder extends Model {
-  static entity = "folders";
+  static entity = 'folders'
 
   static state() {
     return {
       selected: [],
       count: 0,
-      count_shared: 0,
-    };
+      count_shared: 0
+    }
   }
 
-  id!: string;
-  user_id!: string;
-  name!: string;
-  photos_count!: number;
-  created_at!: string;
-  updated_at!: string;
+  id!: string
+  user_id!: string
+  name!: string
+  photos_count!: number
+  created_at!: string
+  updated_at!: string
 
-  photo_references: FolderPhoto[] = [];
-  photos: Photo[] = [];
+  photo_references: FolderPhoto[] = []
+  photos: Photo[] = []
 
   static fields() {
     return {
       id: this.attr(null),
       user_id: this.attr(null),
-      name: this.attr(""),
+      name: this.attr(''),
       photos_count: this.attr(0),
-      created_at: this.attr(""),
-      updated_at: this.attr(""),
+      created_at: this.attr(''),
+      updated_at: this.attr(''),
 
-      photo_references: this.hasMany(FolderPhoto, "folder_id"),
-      photos: this.hasManyThrough(Photo, FolderPhoto, "folder_id", "photo_id"),
-    };
+      photo_references: this.hasMany(FolderPhoto, 'folder_id'),
+      photos: this.hasManyThrough(Photo, FolderPhoto, 'folder_id', 'photo_id')
+    }
   }
 
   static index() {
-    return this.api().get(process.env.VUE_APP_BASE_URL + "/api/folders");
+    return this.api().get(process.env.VUE_APP_BASE_URL + '/api/folders')
   }
 
   static show(folder_id: string) {
     return this.api().get(
-      process.env.VUE_APP_BASE_URL + "/api/folders/" + folder_id
-    );
+      process.env.VUE_APP_BASE_URL + '/api/folders/' + folder_id
+    )
   }
 
   static put(folder_id: string, data: any) {
     return this.api().put(
-      process.env.VUE_APP_BASE_URL + "/api/folders/" + folder_id,
+      process.env.VUE_APP_BASE_URL + '/api/folders/' + folder_id,
       data
-    );
+    )
   }
 
   static post(data: any) {
-    return this.api().post(process.env.VUE_APP_BASE_URL + "/api/folders", data);
+    return this.api().post(process.env.VUE_APP_BASE_URL + '/api/folders', data)
   }
 
   static destroy(folder_id: string) {
     return this.api().delete(
-      process.env.VUE_APP_BASE_URL + "/api/folders/" + folder_id
-    );
+      process.env.VUE_APP_BASE_URL + '/api/folders/' + folder_id
+    )
   }
 
   static destroyPhoto(folder_id: string, reference_id: string) {
     return this.api().delete(
-      process.env.VUE_APP_BASE_URL + "/api/folders/" + folder_id + "/references/" + reference_id
+      process.env.VUE_APP_BASE_URL +
+        '/api/folders/' +
+        folder_id +
+        '/references/' +
+        reference_id
     )
   }
 }
--- src/models/Photo.ts
+++ src/models/Photo.ts
@@ -1,25 +1,25 @@
-import api from "@/api";
-import _default, { Fields, Model } from "@vuex-orm/core";
-import axios from "axios";
+import api from '@/api'
+import _default, { Fields, Model } from '@vuex-orm/core'
+import axios from 'axios'
 
 export default class Photo extends Model {
-  static entity = "photos";
+  static entity = 'photos'
 
-  id!: string;
-  user_id!: string;
-  path!: string;
-  width!: number;
-  height!: number;
-  created_at!: string;
-  updated_at!: string;
+  id!: string
+  user_id!: string
+  path!: string
+  width!: number
+  height!: number
+  created_at!: string
+  updated_at!: string
 
-  thumbnail_loaded!: boolean;
-  thumbnail_loading!: boolean;
-  thumbnail_base64!: string | undefined;
+  thumbnail_loaded!: boolean
+  thumbnail_loading!: boolean
+  thumbnail_base64!: string | undefined
 
-  image_loaded!: boolean;
-  image_loading!: boolean;
-  image_base64!: string | undefined;
+  image_loaded!: boolean
+  image_loading!: boolean
+  image_base64!: string | undefined
 
   static state() {
     return {
@@ -33,19 +33,19 @@ export default class Photo extends Model {
       disk_used_percent: 0,
       disk_used: 0,
       disk_total: 0,
-      scrollTop: 0,
-    };
+      scrollTop: 0
+    }
   }
 
   static fields(): Fields {
     return {
       id: this.attr(null),
       user_id: this.attr(null),
-      path: this.attr(""),
+      path: this.attr(''),
       width: this.attr(0),
       height: this.attr(0),
-      created_at: this.attr(""),
-      updated_at: this.attr(""),
+      created_at: this.attr(''),
+      updated_at: this.attr(''),
 
       thumbnail_loaded: this.attr(false),
       thumbnail_loading: this.attr(false),
@@ -53,105 +53,105 @@ export default class Photo extends Model {
 
       image_loaded: this.attr(false),
       image_loading: this.attr(false),
-      image_base64: this.attr(null),
-    };
+      image_base64: this.attr(null)
+    }
   }
 
   static index(page: number) {
     return this.api()
-      .get(process.env.VUE_APP_BASE_URL + "/api/photos", {
+      .get(process.env.VUE_APP_BASE_URL + '/api/photos', {
         params: {
-          page: page,
-        },
+          page: page
+        }
       })
       .then((response) => {
-        console.log("photos: " + response.response.data.length);
+        console.log('photos: ' + response.response.data.length)
         Photo.commit((state) => {
-          state.page += 1;
-          console.log("page: " + state.page);
-        });
-        return response;
-      });
+          state.page += 1
+          console.log('page: ' + state.page)
+        })
+        return response
+      })
   }
 
   static show(id: string) {
-    return this.api().get(process.env.VUE_APP_BASE_URL + "/api/photos/" + id);
+    return this.api().get(process.env.VUE_APP_BASE_URL + '/api/photos/' + id)
   }
 
   static stats() {
     return this.api()
-      .get(process.env.VUE_APP_BASE_URL + "/api/statistics/summary", {
-        save: false,
+      .get(process.env.VUE_APP_BASE_URL + '/api/statistics/summary', {
+        save: false
       })
       .then((response) => {
         Photo.commit((state) => {
-          state.count = response.response.data.photos;
+          state.count = response.response.data.photos
           state.disk_used_percent =
-            response.response.data.disk_usage.used_percent;
-          state.disk_used = response.response.data.disk_usage.used_size;
-          state.disk_total = response.response.data.disk_usage.total_size;
-        });
-        return response;
-      });
+            response.response.data.disk_usage.used_percent
+          state.disk_used = response.response.data.disk_usage.used_size
+          state.disk_total = response.response.data.disk_usage.total_size
+        })
+        return response
+      })
   }
 
   static upload(file: File) {
-    const formData = new FormData();
-    formData.append("photo", file);
+    const formData = new FormData()
+    formData.append('photo', file)
     const headers = {
-      "Content-Type": "multipart/form-data",
-      Accept: "application/json",
-    };
+      'Content-Type': 'multipart/form-data',
+      Accept: 'application/json'
+    }
     return this.api()
-      .post(process.env.VUE_APP_BASE_URL + "/api/photos", formData, { headers })
+      .post(process.env.VUE_APP_BASE_URL + '/api/photos', formData, { headers })
       .then((response) => {
         Photo.commit((state) => {
-          state.count += 1;
-        });
-        return response;
-      });
+          state.count += 1
+        })
+        return response
+      })
   }
 
   static destroy(id: string) {
     return this.api()
-      .delete(process.env.VUE_APP_BASE_URL + "/api/photos/" + id)
+      .delete(process.env.VUE_APP_BASE_URL + '/api/photos/' + id)
       .then((response) => {
         Photo.commit((state) => {
-          state.count -= 1;
-        });
-        return response;
-      });
+          state.count -= 1
+        })
+        return response
+      })
   }
 
   // lazy load image
   get image() {
     if (this.image_loading || this.image_loaded) {
-      return this.image_base64;
+      return this.image_base64
     }
-    console.log("loading image");
-    this.image_loading = true;
-    this.thumbnail_loading = true;
-    this.$save();
+    console.log('loading image')
+    this.image_loading = true
+    this.thumbnail_loading = true
+    this.$save()
 
     api
       .get(this.path, {
         params: {
-          format: "base64",
-        },
+          format: 'base64'
+        }
       })
       .then((response) => {
-        this.image_base64 = `data:image;base64,${response.data}`;
-        this.thumbnail_base64 = this.image_base64;
-        this.image_loaded = true;
-        this.thumbnail_loaded = true;
-        this.image_loading = false;
-        this.thumbnail_loading = false;
-        this.$save();
-      });
-    return this.image_base64;
+        this.image_base64 = `data:image;base64,${response.data}`
+        this.thumbnail_base64 = this.image_base64
+        this.image_loaded = true
+        this.thumbnail_loaded = true
+        this.image_loading = false
+        this.thumbnail_loading = false
+        this.$save()
+      })
+    return this.image_base64
   }
 
   get thumbnail() {
-    return this.image;
+    return this.image
   }
 }
